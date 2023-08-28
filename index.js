3d63185db73ccea18d03ce3975f0798a6cfadd60
Cooper <55swimmer55@gmail.com>
2023-08-28 12:24:40 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  const midDistanceFee = 'That will be twenty bucks.';
+  const longDistanceFee = 'I will gladly take your thirty bucks.';
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 400 && ride <= 2000) {
+    return midDistanceFee;
+  } else if (ride > 2000 && ride <= 2500) {
+    return longDistanceFee;
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
